5f24a2f3661235008386086377db1e32eaa22b87
Juan José Eusebio <juanerpgr@gmail.com>
2025-01-16 15:17:53 -0800

    ajuste de login

--- utils/constants/env.config.ts
+++ utils/constants/env.config.ts
@@ -1,6 +1,7 @@
 // config.ts
 export const EnvConfig = () => ({
-  api: process.env.NEXT_PUBLIC_API_URL,
+  api: (process.env.NEXT_PUBLIC_API_URL =
+    "https://api.mercadosliz.com:29010/api/v1/"),
   mode: process.env.NEXT_PUBLIC_MODE,
   itemsPerPage: process.env.ITEMS_PER_PAGE, // Note: This will only be available server-side
 });
