bc2301b4f763928b8611efc42abc882d418b73e5
Juan José Eusebio <juanerpgr@gmail.com>
2025-01-24 17:29:15 -0800

    clear login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 import MainForm from "@/components/form/main-form";
 import Providers from "@/hooks/provider";
-/* import Badge from "@/components/badge"; */
 import Layout from "@/templates/layout";
-//
 import { LogInField } from "@/utils/constants/forms/logIn";
 import { useRouter } from "next/navigation";
 
