05f618d3016ac8e05f386dde687ab4302998d645
Juan José Eusebio <juanerpgr@gmail.com>
2025-01-28 17:24:57 -0800

    build

--- hooks/data/load-data.ts
+++ hooks/data/load-data.ts
@@ -1,9 +1,9 @@
 import {
   useGetAlmacenQuery,
-  useGetComprasQuery,
-  useGetMermasQuery,
+  useGetComprasQuery /* 
+  useGetMermasQuery, */,
   useGetMovimientosQuery,
-  useGetVentasQuery,
+  /* useGetVentasQuery, */
   useGetAutocompletarComprasQuery,
   useGetAutocompletarVentasQuery,
   useGetGlosarioComprasQuery,
@@ -22,20 +22,20 @@ export function useQueryByType(
     refetchOnMountOrArgChange: true,
   });
 
-  const ventasQuery = useGetVentasQuery(queryString, {
+  /* const ventasQuery = useGetVentasQuery(queryString, {
     skip: selectedQueryType !== "get-ventas",
     refetchOnMountOrArgChange: true,
-  });
+  }); */
 
   const almacenQuery = useGetAlmacenQuery(queryString, {
     skip: selectedQueryType !== "get-almacen",
     refetchOnMountOrArgChange: true,
   });
-
+  /* 
   const mermasQuery = useGetMermasQuery(queryString, {
     skip: selectedQueryType !== "get-mermas",
     refetchOnMountOrArgChange: true,
-  });
+  }); */
 
   const movimientosQuery = useGetMovimientosQuery(queryString, {
     skip: selectedQueryType !== "get-movimientos",
@@ -45,9 +45,9 @@ export function useQueryByType(
   // Mapear resultados dinámicamente
   const queryResults: Record<string, any> = {
     "get-compras": comprasQuery,
-    "get-ventas": ventasQuery,
+    /* "get-ventas": ventasQuery, */
     "get-almacen": almacenQuery,
-    "get-mermas": mermasQuery,
+    /* "get-mermas": mermasQuery, */
     "get-movimientos": movimientosQuery,
   };
 
