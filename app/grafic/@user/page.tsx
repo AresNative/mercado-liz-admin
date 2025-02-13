9c9772f39327a0f38169d240517d5432cc3edd4c
Juan José Eusebio <juanerpgr@gmail.com>
2025-02-13 15:08:59 -0800

    feat: eliminar lógica de carga y renderizado de gráficos en el componente Estatico

--- app/grafic/@user/page.tsx
+++ app/grafic/@user/page.tsx
@@ -11,57 +11,6 @@ export interface ChartData {
 }
 
 export default function Estatico() {
-    const [chartType, setChartType] = useState<RenderChartProps["type"]>("bar");
-    const [previewData, setPreviewData] = useState<ChartData[]>([]);
-    const [getVentas] = useGetVentasMutation({});
-    async function load() {
-        const dataFilter: formatLoadDate = {
-            filters: {
-                filtros: [],
-                sumas: [{ key: "Nombre" }],
-            },
-            page: 1,
-            pageSize: 5,
-            sum: true,
-        };
 
-        const response: ChartData[] = await loadDataGrafic(getVentas, dataFilter, "Nombre") ?? [];
-        console.log(response);
-        setPreviewData(response);
-    }
-    useEffect(() => {
-        load()
-    }, []);
-
-    return (
-        <>
-            <MainForm
-                actionType=""
-                dataForm={[{
-                    id: 0,
-                    type: "SELECT",
-                    name: "select_grafic",
-                    label: "Tipo de grafica",
-                    options: [
-                        "pie", "bar", "line", "area", "treemap"
-                    ],
-                    multi: false,
-                    require: false,
-                }]}
-                valueAssign="select_grafic"
-                message_button="Cargar"
-                action={(data) => {
-                    setChartType(() => data)
-                }}
-            />
-            <section className="mt-2 p-2 bg-white shadow-2xl rounded-lg">
-                <RenderChart
-                    type={chartType}
-                    barData={previewData}
-                    treemapData={previewData}
-                />
-            </section>
-        </>
-    );
 }
 
